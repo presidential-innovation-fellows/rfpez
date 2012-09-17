253d486ecefbb5b179ecc8fa8b39df4334b5086e
adamjacobbecker <ad@mbecker.cc>
2012-09-17 13:13:13 -0400

    enable bob bundle

--- application/bundles.php
+++ application/bundles.php
@@ -14,7 +14,7 @@
 | Laravel the location of the bundle's root directory, as well as the
 | root URI the bundle responds to.
 |
-| For example, if you have an "admin" bundle located in "bundles/admin" 
+| For example, if you have an "admin" bundle located in "bundles/admin"
 | that you want to handle requests with URIs that begin with "admin",
 | simply add it to the array like this:
 |
@@ -36,5 +36,6 @@
 return array(
 
 	'docs' => array('handles' => 'docs'),
+  'bob'
 
 );
