c526269e46b1f96910774281b351eb65927ab491
adamjacobbecker <ad@mbecker.cc>
2012-09-18 16:21:18 -0400

    add js and css

--- application/views/layout.php
+++ application/views/layout.php
@@ -6,13 +6,15 @@
 <head>
 
   <title></title>
-
+  <?= Basset::show('website.css') ?>
 
 </head>
 <body>
 
+  <?= Section::yield('content') ?>
 
-    <?= Section::yield('content') ?>
+  <!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script> -->
+  <script>window.jQuery || document.write('<script src="/js/jquery-1.8.1.min.js"><\/script>')</script>
 
 </body>
 </html>
