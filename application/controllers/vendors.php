ea92e81c8a2123d5eaf8b84da620e322bbb98cf8
adamjacobbecker <ad@mbecker.cc>
2012-09-18 16:35:36 -0400

    remove big ol' comment

--- application/controllers/vendors.php
+++ application/controllers/vendors.php
@@ -2,34 +2,6 @@
 
 class Vendors_Controller extends Base_Controller {
 
-  /*
-  |--------------------------------------------------------------------------
-  | The Default Controller
-  |--------------------------------------------------------------------------
-  |
-  | Instead of using RESTful routes and anonymous functions, you might wish
-  | to use controllers to organize your application API. You'll love them.
-  |
-  | This controller responds to URIs beginning with "home", and it also
-  | serves as the default controller for the application, meaning it
-  | handles requests to the root of the application.
-  |
-  | You can respond to GET requests to "/home/profile" like so:
-  |
-  |   public function action_profile()
-  |   {
-  |     return "This is your profile!";
-  |   }
-  |
-  | Any extra segments are passed to the method as parameters:
-  |
-  |   public function action_profile($id)
-  |   {
-  |     return "This is the profile for user {$id}.";
-  |   }
-  |
-  */
-
   public function action_new() {
     $view = View::make('vendors.new');
     $this->layout->content = $view;
