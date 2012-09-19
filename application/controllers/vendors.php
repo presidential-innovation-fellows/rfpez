4ecc48da22659d999c53116e5bcaa990fc57cef0
adamjacobbecker <ad@mbecker.cc>
2012-09-19 17:23:53 -0400

    don't memoize validator in controller
    we'll probably want to make some changes to laravel's core at some
    point.

--- application/controllers/vendors.php
+++ application/controllers/vendors.php
@@ -10,16 +10,13 @@ class Vendors_Controller extends Base_Controller {
   public function action_create() {
     $user = new User(Input::get('user'));
     $vendor = new Vendor(Input::get('vendor'));
-    $userValidator = $user->validator();
-    $vendorValidator = $vendor->validator();
-
 
     if ($user->validator()->passes() && $vendor->validator()->passes()) {
       $user->save();
       $user->vendor()->insert($vendor);
       return 'saved';
     } else {
-      $allErrors = array_merge($userValidator->errors->all(), $vendorValidator->errors->all());
+      $allErrors = array_merge($user->validator()->errors->all(), $vendor->validator()->errors->all());
       return Redirect::to_route('new_vendors')->with('errors', $allErrors);
     }
   }
