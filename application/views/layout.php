3fdaabb53fba10d33ef17dbe0306b82c4f258b50
adamjacobbecker <ad@mbecker.cc>
2012-09-23 11:34:38 -0700

    uncomment cdn jquery

--- application/views/layout.php
+++ application/views/layout.php
@@ -34,7 +34,7 @@
 
   <?= Section::yield('content') ?>
 
-  <!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script> -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="/js/jquery-1.8.1.min.js"><\/script>')</script>
 
 </body>
