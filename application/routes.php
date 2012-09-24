d9e5b2d2ed0b01ef70f9743bcf455fa7726b01d0
adamjacobbecker <ad@mbecker.cc>
2012-09-24 09:42:23 -0500

    remove ridiculously long comment

--- application/routes.php
+++ application/routes.php
@@ -1,37 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Simply tell Laravel the HTTP verbs and URIs it should respond to. It is a
-| breeze to setup your application using Laravel's RESTful routing and it
-| is perfectly suited for building large applications and simple APIs.
-|
-| Let's respond to a simple GET request to http://example.com/hello:
-|
-|   Route::get('hello', function()
-|   {
-|     return 'Hello World!';
-|   });
-|
-| You can even respond to more than one URI:
-|
-|   Route::post(array('hello', 'world'), function()
-|   {
-|     return 'Hello World!';
-|   });
-|
-| It's easy to allow URI wildcards using (:num) or (:any):
-|
-|   Route::put('hello/(:any)', function($name)
-|   {
-|     return "Welcome, $name.";
-|   });
-|
-*/
-
 Route::get('/', array('uses' => 'home@index', 'as' => 'root'));
 
 Route::get('signin', array('uses' => 'auth@new', 'as' => 'signin'));
