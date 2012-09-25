c29b371292eb44b1d0ec03c165ae223e6d9c3030
adamjacobbecker <ad@mbecker.cc>
2012-09-25 16:34:30 -0400

    handle dismissed bids on review page

--- application/views/bids/review.php
+++ application/views/bids/review.php
@@ -3,6 +3,12 @@
 
 <h3>Review Bids for <?= $contract->title ?></h3>
 
+<?php if (!Input::get('show_all')): ?>
+  <a href="<?= route('bids', array($contract->id)) ?>?show_all=true">show all bids (including dismissed)</a>
+<?php else: ?>
+  <a href="<?= route('bids', array($contract->id)) ?>">hide dismissed bids</a>
+<?php endif; ?>
+
 <?= View::make('bids.dismiss_modal') ?>
 
 <?php foreach($bids as $bid): ?>
@@ -10,9 +16,14 @@
     $<?= intval($bid->total_price()) ?> - <?= $bid->vendor->company_name ?> |
     <a href="<?= route('bid', array($contract->id, $bid->id)) ?>">details</a>
 
-    | <a href="#" class="show-dismiss-modal" data-contract-id="<?= $contract->id ?>"
+    |
+    <?php if($bid->dismissed()): ?>
+      dismissed
+    <?php else: ?>
+      <a href="#" class="show-dismiss-modal" data-contract-id="<?= $contract->id ?>"
          data-bid-id="<?= $bid->id ?>" data-vendor-company-name="<?= $bid->vendor->company_name ?>"
          data-remove-from-list="bid">Dismiss?</a>
+    <?php endif; ?>
 
   </div>
 <?php endforeach; ?>
