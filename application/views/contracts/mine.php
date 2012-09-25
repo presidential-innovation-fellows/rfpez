ecbf2f94164bfdd5af608aaf2a3c81f66d8d1585
adamjacobbecker <ad@mbecker.cc>
2012-09-25 11:52:38 -0400

    add link to view bids

--- application/views/contracts/mine.php
+++ application/views/contracts/mine.php
@@ -4,7 +4,8 @@
 <h3>My Contracts</h3>
 
 <?php foreach($contracts as $contract): ?>
-  <a href="<?= route('contract', array($contract->id)) ?>"><?= $contract->title ?></a>
+  <a href="<?= route('contract', array($contract->id)) ?>"><?= $contract->title ?></a> |
+  <a href="<?= route('bids', array($contract->id)) ?>">View Bids</a>
 
   <br />
 <?php endforeach; ?>
