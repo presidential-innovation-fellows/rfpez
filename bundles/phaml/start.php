411ed69280e9832ee1b06f1385d9b9be2eaff48e
Taylor Otwell <taylorotwell@gmail.com>
2012-09-26 14:52:38 -0400

    Fixes to HAML stuff.

--- bundles/phaml/start.php
+++ bundles/phaml/start.php
@@ -16,18 +16,10 @@ Event::listen(View::engine, function($view)
 		return $view->get();
 	}
 
-	$contents = file_get_contents($view->path);
+	$contents = $view->get();
 
 	// Create the MtHaml environment which will compiled our PHP Haml
 	$haml = new MtHaml\Environment('php');
 
-	$compiled = $haml->compileString($contents, $view->path);
-
-	// Once we have compiled the file we will write out the compiled version to disk
-	file_put_contents($path = path('storage').'views/'.md5($view->path), $compiled);
-
-	$view->path = $path;
-
-	// We overrode the view path with the compiled path, so we'll just render...
-	return $view->get();
+	return $haml->compileString($contents, $view->path);
 });
