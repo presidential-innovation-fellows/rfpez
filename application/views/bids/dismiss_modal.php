0e637433928f81383d2fdf53f22f7627532536a7
adamjacobbecker <ad@mbecker.cc>
2012-09-25 20:37:17 -0400

    animate modal. why not?

--- application/views/bids/dismiss_modal.php
+++ application/views/bids/dismiss_modal.php
@@ -1,4 +1,4 @@
-<div class="modal hide" id="dismiss-modal" tabindex="-1" role="dialog">
+<div class="modal hide fade" id="dismiss-modal" tabindex="-1" role="dialog">
   <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal">&times;</button>
     <h3>Dismiss bid from <span class="company-name"></span></h3>
