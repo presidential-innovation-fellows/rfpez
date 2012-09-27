1d06899271a6ce3fa2ef65bb5ccece8f244b1348
adamjacobbecker <ad@mbecker.cc>
2012-09-27 16:15:21 -0400

    update database host in root config

--- application/config/database.php
+++ application/config/database.php
@@ -69,7 +69,7 @@ return array(
 
 		'mysql' => array(
 			'driver'   => 'mysql',
-			'host'     => 'localhost',
+			'host'     => '127.0.0.1',
 			'database' => 'rfpez',
 			'username' => 'root',
 			'password' => '',
