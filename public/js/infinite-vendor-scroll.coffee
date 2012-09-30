6db206176a94fe11ec87ef307a5a16031641630c
adamjacobbecker <ad@mbecker.cc>
2012-09-29 23:15:39 -0400

    clean up infinite scroll, fix small bug.

--- public/js/infinite-vendor-scroll.coffee
+++ public/js/infinite-vendor-scroll.coffee
@@ -2,29 +2,29 @@ loading_more_vendors = false
 finished_loading_vendors = false
 
 check_for_scroll_position = ->
-  vendors_div = $('.vendors')
-  bottom_of_vendors = vendors_div.offset().top + vendors_div.height()
-  current_scroll_position = $(document).scrollTop() + $(window).height()
+  bottom_of_vendors = $('.vendors').offset().top + $('.vendors').height()
+  current_scroll_position = $(window).scrollTop() + $(window).height()
   load_more_vendors() if current_scroll_position >= (bottom_of_vendors - 500)
 
 load_more_vendors = ->
   return if loading_more_vendors is true or finished_loading_vendors is true
+
   loading_more_vendors = true
   vendors_div = $('.vendors')
   vendors_wrapper = $('.vendors-wrapper')
   vendors_wrapper.addClass('loading')
-  current_page = vendors_wrapper.data('current-page') || 1
-  next_page = current_page + 1
+  next_page = (vendors_wrapper.data('current-page') || 1) + 1
+
   $.ajax
     url: "/vendors?page=#{next_page}"
     success: (data) ->
-      new_vendors = $(data).find(".vendors")
+      new_vendors = $(data).find(".vendors .vendor")
       vendors_wrapper.removeClass('loading')
       loading_more_vendors = false
       vendors_wrapper.data('current-page', next_page)
       vendors_div.append(new_vendors)
 
-      if new_vendors.children(".vendor").length <= 9
+      if new_vendors.length <= 9
         finished_loading_vendors = true
         vendors_wrapper.addClass('finished-loading')
 
--- public/js/infinite-vendor-scroll.js
+++ public/js/infinite-vendor-scroll.js
@@ -7,17 +7,16 @@
   finished_loading_vendors = false;
 
   check_for_scroll_position = function() {
-    var bottom_of_vendors, current_scroll_position, vendors_div;
-    vendors_div = $('.vendors');
-    bottom_of_vendors = vendors_div.offset().top + vendors_div.height();
-    current_scroll_position = $(document).scrollTop() + $(window).height();
+    var bottom_of_vendors, current_scroll_position;
+    bottom_of_vendors = $('.vendors').offset().top + $('.vendors').height();
+    current_scroll_position = $(window).scrollTop() + $(window).height();
     if (current_scroll_position >= (bottom_of_vendors - 500)) {
       return load_more_vendors();
     }
   };
 
   load_more_vendors = function() {
-    var current_page, next_page, vendors_div, vendors_wrapper;
+    var next_page, vendors_div, vendors_wrapper;
     if (loading_more_vendors === true || finished_loading_vendors === true) {
       return;
     }
@@ -25,18 +24,17 @@
     vendors_div = $('.vendors');
     vendors_wrapper = $('.vendors-wrapper');
     vendors_wrapper.addClass('loading');
-    current_page = vendors_wrapper.data('current-page') || 1;
-    next_page = current_page + 1;
+    next_page = (vendors_wrapper.data('current-page') || 1) + 1;
     return $.ajax({
       url: "/vendors?page=" + next_page,
       success: function(data) {
         var new_vendors;
-        new_vendors = $(data).find(".vendors");
+        new_vendors = $(data).find(".vendors .vendor");
         vendors_wrapper.removeClass('loading');
         loading_more_vendors = false;
         vendors_wrapper.data('current-page', next_page);
         vendors_div.append(new_vendors);
-        if (new_vendors.children(".vendor").length <= 9) {
+        if (new_vendors.length <= 9) {
           finished_loading_vendors = true;
           return vendors_wrapper.addClass('finished-loading');
         }
