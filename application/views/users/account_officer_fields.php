1ff2648207c6cc971343eb8de0013ac1519d808f
adamjacobbecker <ad@mbecker.cc>
2012-09-30 20:14:30 -0400

    jadify officers/new

--- application/views/users/account_officer_fields.php
+++ application/views/users/account_officer_fields.php
@@ -3,21 +3,21 @@
 ?>
 
 <?php if (isset($signup) && $signup): ?>
-  <label>Email:</label>
+  <label>Email</label>
   <input type="text" name="user[email]" value="<?= $user["email"] ?>" />
 <?php endif; ?>
 
-<label>Name:</label>
+<label>Name</label>
 <input type="text" name="officer[name]" value="<?= $officer["name"] ?>" />
 
-<label>Title:</label>
+<label>Title</label>
 <input type="text" name="officer[title]" value="<?= $officer["title"] ?>" />
 
-<label>Agency:</label>
+<label>Agency</label>
 <input type="text" name="officer[agency]" value="<?= $officer["agency"] ?>" />
 
-<label>Phone:</label>
+<label>Phone</label>
 <input type="text" name="officer[phone]" value="<?= $officer["phone"] ?>" />
 
-<label>Fax:</label>
+<label>Fax</label>
 <input type="text" name="officer[fax]" value="<?= $officer["fax"] ?>" />
