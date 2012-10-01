8cd7371d371fb7a74fae627d4c9804b2ea3aab55
Jed Wood <jed@limechile.com>
2012-10-01 16:23:47 -0400

    flip-flop the columns on the new vendor form

--- application/views/users/account_vendor_fields.php
+++ application/views/users/account_vendor_fields.php
@@ -13,42 +13,8 @@
 ?>
 
 <div class="row">
-  <fieldset class="span5">
-    <h3>Contact Info</h3>
 
-    <?php if (isset($signup) && $signup): ?>
-      <label>Email</label>
-      <input type="text" name="user[email]" value="<?= isset($user) ? $user["email"] : "" ?>" />
-
-      <label>Choose a Password</label>
-      <input type="password" name="user[password]" />
-    <?php endif; ?>
-
-    <label>Company Name</label>
-    <input type="text" name="vendor[company_name]" value="<?= $vendor["company_name"] ?>" />
-
-    <label>Person to Contact</label>
-    <input type="text" name="vendor[contact_name]" value="<?= $vendor["contact_name"] ?>" />
-
-    <label>Address</label>
-    <input type="text" name="vendor[address]" value="<?= $vendor["address"] ?>" />
-
-    <label>City</label>
-    <input type="text" name="vendor[city]" value="<?= $vendor["city"] ?>" />
-
-    <label>State</label>
-    <select name="vendor[state]">
-      <?php foreach(Helper::all_us_states() as $code => $state): ?>
-        <option value="<?= $code ?>" <?php if ($vendor["state"] == $code) echo "selected"; ?>><?= $state ?></option>
-      <?php endforeach; ?>
-    </select>
-
-  <label>Zip</label>
-  <input type="text" name="vendor[zip]" value="<?= $vendor["zip"] ?>" />
-
-  </fieldset>
-
-  <fieldset class="span5">
+    <fieldset class="span5">
     <h3>Work and Capabilities</h3>
 
     <label><h5>Your company in 50 words or less</h5></label>
@@ -88,6 +54,41 @@
         <img />
       </div>
     </div>
+  </fieldset>
+
+  <fieldset class="span5">
+    <h3>Contact Info</h3>
+
+    <?php if (isset($signup) && $signup): ?>
+      <label>Email</label>
+      <input type="text" name="user[email]" value="<?= isset($user) ? $user["email"] : "" ?>" />
+
+      <label>Choose a Password</label>
+      <input type="password" name="user[password]" />
+    <?php endif; ?>
+
+    <label>Company Name</label>
+    <input type="text" name="vendor[company_name]" value="<?= $vendor["company_name"] ?>" />
+
+    <label>Person to Contact</label>
+    <input type="text" name="vendor[contact_name]" value="<?= $vendor["contact_name"] ?>" />
+
+    <label>Address</label>
+    <input type="text" name="vendor[address]" value="<?= $vendor["address"] ?>" />
+
+    <label>City</label>
+    <input type="text" name="vendor[city]" value="<?= $vendor["city"] ?>" />
+
+    <label>State</label>
+    <select name="vendor[state]">
+      <?php foreach(Helper::all_us_states() as $code => $state): ?>
+        <option value="<?= $code ?>" <?php if ($vendor["state"] == $code) echo "selected"; ?>><?= $state ?></option>
+      <?php endforeach; ?>
+    </select>
+
+    <label>Zip</label>
+    <input type="text" name="vendor[zip]" value="<?= $vendor["zip"] ?>" />
+
   </fieldset>
 
 </div>
