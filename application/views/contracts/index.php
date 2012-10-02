e6ab717cdac0f83b07bfd74d9b38d0e00d8ed1f5
adamjacobbecker <ad@mbecker.cc>
2012-10-01 22:14:34 -0400

    more jadification

--- application/views/contracts/index.php
+++
@@ -1,9 +0,0 @@
-<?php Section::inject('page_title', 'Contracts'); ?>
-
-<h1>Contracts</h1>
-
-<?php foreach($contracts as $contract): ?>
-  <a href="<?= route('contract', array($contract->id)) ?>"><?= $contract->title ?></a>
-
-  <br />
-<?php endforeach; ?>
