b8d4e7fb17367891d4cf429f99772d9fa51768cb
Jed Wood <jed@limechile.com>
2012-10-03 17:10:22 -0400

    just until Adam gets a dashboard going, set home page to contracts or 'my contracts' for logged-in users

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,7 +4,13 @@ class Home_Controller extends Base_Controller {
 
   public function action_index() {
     if (Auth::check()) {
-      $view = View::make('home.index_signed_in');
+      if (Auth::user()->officer) {
+        return Redirect::to_route('my_contracts');
+      } else {
+        return Redirect::to_route('contracts');
+      }
+      // When we have something better...
+      //$view = View::make('home.index_signed_in');
     } else {
       $view = View::make('home.index_signed_out');
     }
