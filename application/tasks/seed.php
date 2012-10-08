e931c57e6d8506fffb92d8898fc6a6e5d52dfb16
adamjacobbecker <ad@mbecker.cc>
2012-10-08 11:19:45 -0400

    seed less users and more contracts

--- application/tasks/seed.php
+++ application/tasks/seed.php
@@ -8,11 +8,11 @@ class Seed_Task {
     Service::create(array('name' => 'Web Development', 'description' => 'This is the description for web development.'));
     Service::create(array('name' => 'Content Management ', 'description' => 'This is the description for content management.'));
 
-    for ($i = 0; $i < 10; $i++) Factory::vendor();
-    for ($i = 0; $i < 10; $i++) Factory::officer();
-    for ($i = 0; $i < 5; $i++) Factory::contract();
-    for ($i = 0; $i < 10; $i++) Factory::bid();
-    for ($i = 0; $i < 10; $i++) Factory::question();
+    for ($i = 0; $i < 5; $i++) Factory::vendor();
+    for ($i = 0; $i < 5; $i++) Factory::officer();
+    for ($i = 0; $i < 10; $i++) Factory::contract();
+    for ($i = 0; $i < 15; $i++) Factory::bid();
+    for ($i = 0; $i < 15; $i++) Factory::question();
 
   }
 
