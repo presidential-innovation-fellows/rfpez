9ad5102af641862109e51a6f7034f54282bd662d
adamjacobbecker <ad@mbecker.cc>
2012-10-12 09:29:50 -0400

    merge branches

--- tests/acceptance/SigninOfficerCept.php
+++ tests/acceptance/SigninOfficerCept.php
@@ -1,10 +1,12 @@
 <?php
-
 $I = new WebGuy($scenario);
-$I->wantTo('sign in as an officer');
-$I->amOnPage('/signin');
-$I->fillField('email','officer1@example.gov');
-$I->fillField('password','password');
-$I->click('button.btn');
+$I->wantTo('sign in as an officer via the modal');
+$I->amOnPage('/');
+$I->click('Sign In', 'a');
+$I->fillField('#signinModal #email','officer1@example.gov');
+$I->fillField('#signinModal input[type=password]','password');
+$I->click('#signinModal button.btn-primary');
+$I->amOnPage('/contracts/mine');
 $I->see('My Contracts', 'h4');
+
 ?>
