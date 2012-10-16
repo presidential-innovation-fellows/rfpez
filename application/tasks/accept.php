a1187298ed93f87cba94e922c3f5db51b5b8f2c8
Jed Wood <jed@limechile.com>
2012-10-16 16:45:37 -0400

    moved test files to standard location. Also, just set the env to test automatically

--- application/tasks/accept.php
+++ application/tasks/accept.php
@@ -4,12 +4,10 @@ class Accept_Task {
 
   public function run()
   {
-    if(!Laravel\Request::is_env('test')) {
-      die("Umm, you probably don't want to run this with anything other than --env=test\n");
-    }
+    Laravel\Request::set_env('test');
 
     Laravel\CLI\Command::run(array('sync'));
-    passthru('php codecept.phar run acceptance --steps --debug');
+    passthru('php application/tests/codecept.phar run acceptance --steps --debug');
   }
 
 }
