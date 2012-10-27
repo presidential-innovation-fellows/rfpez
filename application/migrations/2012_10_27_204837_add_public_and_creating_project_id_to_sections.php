8b7b711140832d38b34728bb64457e1b00b999eb
adamjacobbecker <ad@mbecker.cc>
2012-10-27 16:51:10 -0400

    MIGRATION: add public and created_by_project_id to project_sections table

---
+++ application/migrations/2012_10_27_204837_add_public_and_creating_project_id_to_sections.php
@@ -0,0 +1,35 @@
+<?php
+
+class Add_Public_And_Creating_Project_Id_To_Sections {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('project_sections', function($t){
+			$t->integer('created_by_project_id')->nullable();
+			$t->boolean('public')->default(0);
+
+      $t->foreign('created_by_project_id')->references('id')->on('projects')->on_delete('SET NULL');
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('project_sections', function($t){
+      $t->drop_foreign('project_sections_created_by_project_id_foreign');
+
+			$t->drop_column('created_by_project_id');
+			$t->drop_column('public');
+		});
+	}
+
+}
