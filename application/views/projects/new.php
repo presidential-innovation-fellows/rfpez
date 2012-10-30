7989e994d7245bac5a2b60f5effbd195406a1268
adamjacobbecker <ad@mbecker.cc>
2012-10-29 20:07:36 -0400

    make project/new page less ugly

--- application/views/projects/new.php
+++ application/views/projects/new.php
@@ -1,30 +1,8 @@
 <?php Section::inject('page_title', 'New Project') ?>
-<div class="row-fluid">
-  <div class="span5">
-    <form id="new-project-form" class="form-big" action="<?php echo Jade\Dumper::_text(route('projects')); ?>" method="POST">
-      <div class="control-group">
-        <input type="text" name="project[title]" placeholder="Project Title" />
-      </div>
-      <div class="control-group">
-        <input type="text" name="project[agency]" placeholder="Agency" />
-      </div>
-      <div class="control-group">
-        <input type="text" name="project[office]" placeholder="Office" />
-      </div>
-      <div class="control-group">
-        <select name="project[project_type_id]">
-          <option value="">-- Select Project Type --</option>
-          <?php foreach (ProjectType::all() as $project_type): ?>
-            <option value="<?php echo Jade\Dumper::_text($project_type->id); ?>"><?php echo Jade\Dumper::_text($project_type->name); ?></option>
-          <?php endforeach; ?>
-        </select>
-      </div>
-      <div class="form-actions">
-        <button class="btn btn-primary" type="submit">Create Project</button>
-      </div>
-    </form>
-  </div>
-  <div class="span7 well">
+<?php Section::inject('no_page_header', true) ?>
+<div class="new-project-page">
+  <h4>New Project</h4>
+  <div class="well">
     <p>
       <strong>Congrats on using EasyBid for your procurement!</strong>
     </p>
@@ -34,4 +12,26 @@
       and let us know.
     </p>
   </div>
+  <form id="new-project-form" class="form-big" action="<?php echo Jade\Dumper::_text(route('projects')); ?>" method="POST">
+    <div class="control-group">
+      <input type="text" name="project[title]" placeholder="Project Title" />
+    </div>
+    <div class="control-group">
+      <input type="text" name="project[agency]" placeholder="Agency" />
+    </div>
+    <div class="control-group">
+      <input type="text" name="project[office]" placeholder="Office" />
+    </div>
+    <div class="control-group">
+      <select name="project[project_type_id]">
+        <option value="">-- Select Project Type --</option>
+        <?php foreach (ProjectType::all() as $project_type): ?>
+          <option value="<?php echo Jade\Dumper::_text($project_type->id); ?>"><?php echo Jade\Dumper::_text($project_type->name); ?></option>
+        <?php endforeach; ?>
+      </select>
+    </div>
+    <div class="form-actions">
+      <button class="btn btn-primary" type="submit">Create Project</button>
+    </div>
+  </form>
 </div>
