8e52a9f549a1810b38ef44526f58d16b9d3fa05a
adamjacobbecker <ad@mbecker.cc>
2012-11-02 11:21:21 -0400

    Completes GH-75 Add sidebar to "Post on FBO" page with instructions for non-COs

--- application/views/projects/post_on_fbo.php
+++ application/views/projects/post_on_fbo.php
@@ -3,20 +3,34 @@
 <?php Section::inject('active_subnav', 'post_on_fbo') ?>
 <?php Section::inject('no_page_header', true) ?>
 <?php echo Jade\Dumper::_html(View::make('projects.partials.toolbar')->with('project', $project)); ?>
-<h5>Step 1</h5>
-<p class="readable-width">
-  Create a new notice on FBO like you normally do. Be sure to use a unique solicitation number,
-  which you will need in the next step. When it comes time to enter the body, just
-  copy and paste the text below exactly as-is.
-</p>
-<input class="input-xxlarge" type="text" value="<?php echo Jade\Dumper::_text(View::make('projects.partials.fbo_body')->with('project', $project)); ?>" data-select-text-on-focus="true" />
-<h5>Step 2</h5>
-<p>Enter the solicitation number that you used on FBO.</p>
-<form id="sync-with-fbo-form" method="POST">
-  <div class="control-group">
-    <div class="input-append">
-      <input type="text" name="fbo_solnbr" placeholder="Solitication Number" />
-      <button class="btn btn-primary" data-loading-text="Syncing with FBO...">Sync with FBO</button>
+<div class="row-fluid">
+  <div class="span8">
+    <h5>Step 1</h5>
+    <p class="readable-width">
+      Create a new notice on FBO like you normally do. Be sure to use a unique solicitation number,
+      which you will need in the next step. When it comes time to enter the body, just
+      copy and paste the text below exactly as-is.
+    </p>
+    <input class="input-xxlarge" type="text" value="<?php echo Jade\Dumper::_text(View::make('projects.partials.fbo_body')->with('project', $project)); ?>" data-select-text-on-focus="true" />
+    <h5>Step 2</h5>
+    <p>Enter the solicitation number that you used on FBO.</p>
+    <form id="sync-with-fbo-form" method="POST">
+      <div class="control-group">
+        <div class="input-append">
+          <input type="text" name="fbo_solnbr" placeholder="Solitication Number" />
+          <button class="btn btn-primary" data-loading-text="Syncing with FBO...">Sync with FBO</button>
+        </div>
+      </div>
+    </form>
+  </div>
+  <div class="span4">
+    <div class="well">
+      <h5>Not a Contracting Officer?</h5>
+      <p>
+        This step is for certified contracting officers only. If that's not you,
+        just click on the "admin" tab above and you can invite your CO to
+        collaborate with you.
+      </p>
     </div>
   </div>
-</form>
+</div>
