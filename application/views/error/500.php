a6e023066e376b83d9a4d08ed1beabb7ba59a2b2
Jed Wood <jed@limechile.com>
2012-11-09 11:12:44 -0600

    make 500 page more static with no other js/css calls

--- application/views/error/500.php
+++ application/views/error/500.php
@@ -8,13 +8,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <meta name="description" content="" />
   <meta name="viewport" content="width=device-width" />
-  <title>Page Not Found
-</title>
-  <?php echo Jade\Dumper::_html(HTML::style('http://fonts.googleapis.com/css?family=Telex')); ?>
-  <?php echo Jade\Dumper::_html(Basset::show('website.css')); ?>
+  <title>Server Error</title>
 </head>
 <body>
-  <?php echo Jade\Dumper::_html(View::make('partials.topnav')); ?>
   <div class="container">
     <h2>Server Error.</h2>
     <p class="lead">Hmmmm, something went wrong. We'll try to fix it. Contact us if you have an urgent issue.</p>
