626bac4a249be68d6b09c25727d1b8b44ff552b7
adamjacobbecker <ad@mbecker.cc>
2012-11-20 22:51:27 -0500

    Implement a grunt-based asset build process.
    mas grunt

---
+++ assets/js/dsbs-lookup.js
@@ -0,0 +1,34 @@
+
+$(document).on("ready pjax:success", function() {
+  return $("[data-dsbs-user-id]").each(function() {
+    var el, user_id;
+    el = $(this);
+    user_id = el.data('dsbs-user-id');
+    el.on('load-dsbs', function() {
+      return $.ajax({
+        url: "http://rfpez-apis.presidentialinnovationfellows.org/bizs?user_id=" + user_id,
+        dataType: "json",
+        success: function(data) {
+          var key, result, _results;
+          result = data.results[0];
+          if (!result) {
+            el.removeClass('loading');
+            return el.addClass('no-certs');
+          } else {
+            if (result.user_id === user_id) {
+              el.removeClass('loading');
+              _results = [];
+              for (key in result) {
+                _results.push(el.find("[data-key=" + key + "]").text(result[key]));
+              }
+              return _results;
+            }
+          }
+        }
+      });
+    });
+    if (el.data('defer') === false) {
+      return el.trigger('load-dsbs');
+    }
+  });
+});
