be4800bc2463823d056a7cb137ad91fe23726336
adamjacobbecker <ad@mbecker.cc>
2012-11-21 11:08:11 -0500

    merge

--- application/views/layout.php
+++ application/views/layout.php
@@ -29,7 +29,17 @@
   <script>
     window.jQuery || document.write('<script src="/js/vendor/jquery-1.8.1.min.js"><\/script>')
   </script>
-  <?php echo Jade\Dumper::_html(Helper::asset('js/all')); ?>
+  <?php echo Jade\Dumper::_html(Helper::asset('js/global')); ?>
+  <?php if (Auth::user()): ?>
+    <?php if (Auth::officer() && Auth::officer()->is_role_or_higher(Officer::ROLE_ADMIN)): ?>
+      <?php echo Jade\Dumper::_html(Helper::asset('js/admin')); ?>
+    <?php endif; ?>
+    <?php if (Auth::officer()): ?>
+      <?php echo Jade\Dumper::_html(Helper::asset('js/officer')); ?>
+    <?php else: ?>
+      <?php echo Jade\Dumper::_html(Helper::asset('js/vendor')); ?>
+    <?php endif; ?>
+  <?php endif; ?>
   <?php echo Jade\Dumper::_html(Section::yield('additional_scripts')); ?>
 </head>
 <body class="<?php echo Jade\Dumper::_text($body_class); ?>">
