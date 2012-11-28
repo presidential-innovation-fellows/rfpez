6bd4433175fb294cdc7f5ffa5545e4785a340ecb
adamjacobbecker <ad@mbecker.cc>
2012-11-28 14:59:51 -0500

    chromeframe prompt for < IE 8

--- application/views/layout.php
+++ application/views/layout.php
@@ -43,6 +43,9 @@
   <?php echo Jade\Dumper::_html(Section::yield('additional_scripts')); ?>
 </head>
 <body class="<?php echo Jade\Dumper::_text($body_class); ?>">
+  <!--[if lt IE 8]>
+    <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+  <![endif]-->
   <div id="pjax-container">
     <?php echo Jade\Dumper::_html(View::make('pjaxcontainer')->with('content', $content)); ?>
   </div>
