42fa8cb1308df226cf2e636c762ee541ca7e65b5
adamjacobbecker <ad@mbecker.cc>
2012-11-30 11:50:24 -0500

    get rid of #pjax-container

--- application/views/layout.php
+++ application/views/layout.php
@@ -45,8 +45,37 @@
   <!--[if lt IE 8]>
     <p class="chromeframe"><?= __("r.chromeframe_text") ?></p>
   <![endif]-->
-  <div id="pjax-container">
-    <?php echo View::make('pjaxcontainer')->with('content', $content); ?>
+  <div id="outer-container">
+    <?php echo View::make('partials.topnav'); ?>
+    <div class="container">
+      <?php if (Auth::guest()): ?>
+        <?php echo View::make('partials.signin_modal'); ?>
+      <?php endif; ?>
+      <?php if (Session::has('errors')): ?>
+        <div class="alert alert-error">
+          <button type="button" class="close" data-dismiss="alert">×</button>
+          <ul>
+            <?php foreach(Session::get('errors') as $error): ?>
+              <li><?php echo e($error); ?></li>
+            <?php endforeach; ?>
+          </ul>
+        </div>
+      <?php endif; ?>
+      <?php if (Session::has('notice')): ?>
+        <div class="alert alert-success">
+          <?php echo e(Session::get('notice')); ?>
+          <button type="button" class="close" data-dismiss="alert">×</button>
+        </div>
+      <?php endif; ?>
+      <?php if (!Section::yield('no_page_header')) { ?>
+        <h4>
+          <?php echo e(Section::yield('page_title')); ?>
+          <?php echo Section::yield('inside_header'); ?>
+        </h4>
+      <?php } ?>
+      <?php echo Section::yield('subnav'); ?>
+      <?php echo $content; ?>
+    </div>
   </div>
   <?php echo View::make('partials.footer'); ?>
   <?php if (Request::is_env('production')) { ?>
