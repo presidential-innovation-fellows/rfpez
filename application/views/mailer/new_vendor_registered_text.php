ca580a48976c848a501122d527ce63e90b026699
adamjacobbecker <ad@mbecker.cc>
2012-12-30 15:05:37 -0500

    better new vendor signup email
    @cjoh look-over?

--- application/views/mailer/new_vendor_registered_text.php
+++ application/views/mailer/new_vendor_registered_text.php
@@ -1,5 +1,11 @@
 Hey <?= $user->vendor->company_name ?>,
 
-Thanks for signing up for EasyBid.
+Thanks for signing up for EasyBid, and for being one of the first to participate in this exciting pilot program.
+
+In the coming weeks, we'll be adding more and more government projects that are looking for proposals from businesses like yours. If you checked the box during sign-up, we'll send you notices about these from time-to-time.
+
+We think there are some great synergies where we can help government gain access to the awesome services you provide, while also opening up a whole new market to your business.
+
+If you have any questions or comments, feel free to reply to this email or tweet at @ProjectRFPEZ. And thanks again for signing up for our pilot!
 
 <?= __('r.email_signature_text') ?>
