618b732f3301ac2232a769b746249c769e4c72f1
adamjacobbecker <ad@mbecker.cc>
2013-01-08 16:03:14 -0500

    remove sow composer

---
+++ application/migrations/2013_01_08_204146_remove_sow_composer.php
@@ -0,0 +1,36 @@
+<?php
+
+class Remove_Sow_Composer {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::drop('project_section_type');
+		Schema::drop('project_sections');
+
+		Schema::table('projects', function($t){
+			$t->drop_column('background');
+			$t->drop_column('sections');
+			$t->drop_column('variables');
+			$t->drop_column('sow_progress');
+
+			$t->text('body');
+			$t->string('etherpad_id');
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		throw new \Exception("Can't migrate down.");
+	}
+
+}
--- application/tasks/seed.php
+++ application/tasks/seed.php
@@ -7,17 +7,17 @@ class Seed_Task {
     // Create the base data.
     $this->base_data();
     $this->minimal_data();
-    $project = Project::first();
+    // $project = Project::first();
 
-    // Create a bunch more stuff, just for testin'.
-    for ($i = 0; $i < 5; $i++) Factory::project($project->id);
-    for ($i = 0; $i < 15; $i++) Factory::bid();
-    for ($i = 0; $i < 15; $i++) Factory::question();
-    for ($i = 0; $i < 10; $i++) Factory::vendor();
-    for ($i = 0; $i < 40; $i++) Factory::section();
+    // // Create a bunch more stuff, just for testin'.
+    // for ($i = 0; $i < 5; $i++) Factory::project($project->id);
+    // for ($i = 0; $i < 15; $i++) Factory::bid();
+    // for ($i = 0; $i < 15; $i++) Factory::question();
+    // for ($i = 0; $i < 10; $i++) Factory::vendor();
+    // for ($i = 0; $i < 40; $i++) Factory::section();
 
-    for ($i = 0; $i < 20; $i++) Factory::project($project->id);
-    for ($i = 0; $i < 40; $i++) Factory::bid(array(), $project->id);
+    // for ($i = 0; $i < 20; $i++) Factory::project($project->id);
+    // for ($i = 0; $i < 40; $i++) Factory::bid(array(), $project->id);
 
   }
 
@@ -66,48 +66,6 @@ class Seed_Task {
     $faker = Faker\Factory::create();
     for ($i = 0; $i < 5; $i++) Factory::vendor();
     for ($i = 0; $i < 5; $i++) Factory::officer();
-
-    $project_type_1 = ProjectType::first();
-
-    // Create project sections
-    $section1 = ProjectSection::create(array('section_category' => 'Deliverables',
-                                             'title' => 'Information Architecture',
-                                             'public' => true,
-                                             'body' => 'Information architecture (IA) and initial site map for the redesigned {{WEBSITE}} site. This IA and site map will be used so copy can be revised and/or developed in-house while the contractor is in the process of executing other deliverable products. Note the IA and site map will only include content/sites currently in the {{WEBSITE}} infrastructure.'));
-
-    $section2 = ProjectSection::create(array('section_category' => 'Deliverables',
-                                             'title' => 'Page Templating',
-                                             'public' => true,
-                                             'body' => 'Complete redesign of {{WEBSITE}} and templates for outreach materials. This includes the complete website, features, applications, blog, social media, mobile, email and newsletters, and any items deemed best to connect with the audiences identified in Deliverable Product #1.'));
-
-    $section3 = ProjectSection::create(array('section_category' => 'Objectives',
-                                             'title' => 'Increased Visibility',
-                                             'public' => true,
-                                             'body' => $faker->paragraph));
-
-    // Link project sections to api project type
-    $project_type_1->project_sections()->sync(array($section1->id, $section2->id, $section3->id));
-
-    // Create first project
-    $project = new Project(array('project_type_id' => $project_type_1->id,
-                                     'title' => 'API for SBA.gov Dynamic Small Business Search',
-                                     'agency' => 'Small Business Administration',
-                                     'office' => 'Office of Innovation and Research',
-                                     'recommended' => true,
-                                     'public' => true,
-                                     'background' => $faker->paragraph,
-                                     'sections' => array($section1->id, $section2->id, $section3->id),
-                                     'variables' => array('WEBSITE' => 'api.dsbs.sba.gov'),
-                                     'deliverables' => array('Information Architecture' => '1/1/13', 'Page Templating' => '2/8/13'),
-                                     'proposals_due_at' => new \DateTime('12/31/2012')
-                                     ));
-
-    $project->posted_to_fbo_at = new \DateTime;
-    $project->save();
-
-
-    // ...And give it to officer1
-    $project->officers()->attach(Officer::first()->id, array('owner' => true));
   }
 
 }
