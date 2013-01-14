0caafd031e25faf552715dbc8eaeebf4e9296355
Jed Wood <jed@limechile.com>
2013-01-14 13:21:32 -0500

    a draft that re-orders and heavy edits down the content.

--- application/views/home/government.php
+++ application/views/home/government.php
@@ -1,29 +1,30 @@
 <?php Section::inject('page_title', 'RFP-EZ For Government') ?>
 <hr />
 <p class="lead">
-  RFP-EZ is a new platform that has been created under the
-  <a href="http://wh.gov/innovationfellows" target="_blank">Presidential Innovation Fellows Program</a>
-  to reduce barriers to entry for small businesses -- especially those who are new entrants -- and
-  improve the overall efficiency of small dollar procurements both for the government and small businesses.
+  Make small dollar procurements better for both government and businesses.
 </p>
-<hr />
-<p>
-  This site is a pilot, which will initially run from December 28, 2012 through May 1, 2013,
-  where agencies will identify individual procurements valued at or below the simplified acquisition threshold
-  that can be set aside for small businesses to test a suite of functional tools for:
+<p class="lead">
+  RFP-EZ was created by the <a href="http://wh.gov/innovationfellows" target="_blank">Presidential Innovation Fellows</a> to:
 </p>
-<ol class="extra-bottom-padding">
-  <li>Simplifying the development of statements of work,</li>
-  <li>Improving agency access to information about small businesses,</li>
-  <li>Enabling small businesses to submit proposals electronically,</li>
-  <li>Enhancing efficiencies for evaluating proposals, and</li>
-  <li>Improving how information (including prices paid by federal agencies) is captured and stored.</li>
+<ol class="lead-list extra-bottom-padding">
+  <li>Make it easier for new small businesses to start contracting,</li>
+  <li>Simplify the development of statements of work,</li>
+  <li>Improve agency access to information about small businesses,</li>
+  <li>Enable small businesses to submit proposals electronically,</li>
+  <li>Help officers evaluate proposals more quickly,</li>
+  <li>Improve how information (including prices paid by federal agencies) is captured and stored.</li>
 </ol>
-<blockquote>
-  <p>OFPP and SBA see multiple benefits to this pilot. First, it gives us an opportunity to think about how we can transform the way we conduct SAT procurements, both for agencies and our trading partners.  Second, it gives us another mechanism to potentially increase opportunities for small businesses.  Third, it can serve as a model for innovative contracting where we encourage measured and constructive risk-taking by the workforce to test new ideas for improving the acquisition process.   For these reasons, our community should be as supportive as we can.</p>
-  <small>Matthew Blum, Associate Administrator, Office of Federal Procurement Policy</small>
-</blockquote>
 <p class="lead">
   Ready to try RFP-EZ for yourself?&nbsp;
   <a class="btn btn-large btn-warning" href="<?php echo e(route('new_officers')); ?>">Sign up now</a>
+</p>
+<hr />
+<blockquote class="blum">
+  <p>OFPP and SBA see multiple benefits to this pilot. First, it gives us an opportunity to think about how we can transform the way we conduct SAT procurements, both for agencies and our trading partners.  Second, it gives us another mechanism to potentially increase opportunities for small businesses.  Third, it can serve as a model for innovative contracting where we encourage measured and constructive risk-taking by the workforce to test new ideas for improving the acquisition process.   For these reasons, our community should be as supportive as we can.</p>
+  <small>Matthew Blum, Associate Administrator, Office of Federal Procurement Policy</small>
+</blockquote>
+<hr />
+<p class="small">
+  This site is a pilot, which will initially run from December 28, 2012 through May 1, 2013,
+  where agencies will identify individual procurements valued at or below the simplified acquisition threshold.
 </p>
