522ab177bc739e07f8b51071628b0f456ba8e252
adamjacobbecker <ad@mbecker.cc>
2013-01-15 11:24:12 -0500

    Fix bug where edit link gets turbolink'd

--- application/views/projects/partials/sections_for_editing.php
+++ application/views/projects/partials/sections_for_editing.php
@@ -8,7 +8,7 @@
             <?php foreach ($sections as $section): ?>
               <div class="section" data-section-id="<?php echo e($section->id); ?>" data-section-title="<?php echo e($section->title); ?>" data-will-fork="<?php echo e($section->can_edit_without_forking() ? 'false' : 'true'); ?>">
                 <strong><?php echo e($section->title); ?></strong>
-                <a class="btn btn-success btn-mini edit-section-link" href="#">Edit</a>
+                <a class="btn btn-success btn-mini edit-section-link">Edit</a>
                 <a class="btn btn-danger btn-mini remove-button" data-href="<?php echo e(route('project_section_delete', array($project->id, $section->id))); ?>" data-loading-text="Removing...">Remove</a>
                 <p class="body"><?php echo $section->body; ?></p>
               </div>
