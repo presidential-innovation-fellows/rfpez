27d64f4194cce9afa0a147480003180854832286
adamjacobbecker <ad@mbecker.cc>
2013-01-22 16:07:41 -0500

    grab some data. this is ugly.

--- application/controllers/reports.php
+++ application/controllers/reports.php
@@ -13,6 +13,72 @@ class Reports_Controller extends Base_Controller {
 
   public function action_index() {
     $view = View::make('reports.index');
+
+    $all_days = array();
+    $date = new \DateTime();
+    $date->setTimestamp(1358488800); # 1/18/2013
+
+    while ($date < new \DateTime()) {
+      array_push($all_days, $date->format("Y-m-d"));
+      $date = $date->modify("+1 day");
+    }
+
+    $signups = array();
+    foreach (DB::query("SELECT COUNT(*) as count, DATE(created_at) as date FROM vendors GROUP BY DATE(created_at)") as $result) {
+      $signups[$result->date] = $result->count;
+    }
+
+    $signups_per_day = array();
+    foreach ($all_days as $day) {
+      $signups_per_day[$day] = @$signups[$day] ?: 0;
+    }
+
+    $signups = array();
+    foreach (DB::query("SELECT DATE(created_at) as date, duns FROM vendors") as $result) {
+      if (!isset($signups[$result->date])) $signups[$result->date] = array('new' => 0, 'old' => 0);
+
+      if ($result->duns) {
+        $signups[$result->date]['old']++;
+      } else {
+        $signups[$result->date]['new']++;
+      }
+    }
+
+    $num_signups = 0;
+    $num_new = 0;
+    $new_to_contracting = array();
+    foreach ($all_days as $day) {
+      if (!isset($signups[$day])) continue;
+
+      $num_signups += $signups[$day]['new'];
+      $num_signups += $signups[$day]['old'];
+      $num_new += $signups[$day]['new'];
+
+      $new_to_contracting[$day] = $num_new / $num_signups;
+    }
+
+    $total_projects = 0;
+    $total_bids = 0;
+    $bids_per_project = array();
+    foreach (DB::query("SELECT id, title, (SELECT COUNT(*) from bids where project_id = projects.id) as bids from projects") as $result) {
+      array_push($bids_per_project, array(
+        'project_id' => $result->id,
+        'project_title' => $result->title,
+        'num_bids' => $result->bids
+      ));
+      $total_projects++;
+      $total_bids += $result->bids;
+    }
+
+    $avg_bids_per_project = $total_bids / $total_projects;
+
+
+
+    $view->signups_per_day = $signups_per_day;
+    $view->new_to_contracting = $new_to_contracting;
+    $view->bids_per_project = $bids_per_project;
+    $view->avg_bids_per_project = $avg_bids_per_project;
+
     $this->layout->content = $view;
   }
 
--- application/views/reports/index.php
+++ application/views/reports/index.php
@@ -1,2 +1,10 @@
 <?php Section::inject('page_title', 'Reports') ?>
 <p>Reports!</p>
+<h5>signups_per_day</h5>
+<code><?php echo json_encode($signups_per_day); ?></code>
+<h5>new_to_contracting</h5>
+<code><?php echo json_encode($new_to_contracting); ?></code>
+<h5>bids_per_project</h5>
+<code><?php echo json_encode($bids_per_project); ?></code>
+<h5>avg_bids_per_project</h5>
+<code><?php echo json_encode($avg_bids_per_project); ?></code>
