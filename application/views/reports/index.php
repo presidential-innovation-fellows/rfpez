e800980031b953dbe423994204e3ece2c0e2bd65
adamjacobbecker <ad@mbecker.cc>
2013-01-24 16:58:16 -0500

    force turbolinks to reload for reports by putting script load into document head

--- application/views/reports/index.php
+++ application/views/reports/index.php
@@ -15,7 +15,10 @@
     <div id="price-bids-chart"></div>
   </div>
 </div>
-<script src="https://www.google.com/jsapi?autoload=%7B%22modules%22%3A%5B%7B%22name%22%3A%22visualization%22%2C%22version%22%3A%221%22%2C%22packages%22%3A%5B%22corechart%22%5D%7D%5D%7D"></script>
+<?php Section::start('additional_scripts'); { ?>
+  <script src="https://www.google.com/jsapi?autoload=%7B%22modules%22%3A%5B%7B%22name%22%3A%22visualization%22%2C%22version%22%3A%221%22%2C%22packages%22%3A%5B%22corechart%22%5D%7D%5D%7D"></script>
+<?php } ?>
+<?php Section::stop(); ?>
 <script>
   Rfpez.reporting_stats({ bidsPerProject : <?php echo json_encode($bids_per_project); ?>, avgPrices: <?php echo json_encode($avg_prices); ?>, avgPriceTotal : <?php echo json_encode($avg_price_total); ?> });
 </script>
