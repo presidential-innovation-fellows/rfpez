b3e08f09641520eeb8545bf7d5b6b7428ed888c4
Aaron Snow <aaron@daringapps.com>
2013-07-24 00:34:29 -0400

    update display for new officer signup

--- application/views/officers/new.php
+++ application/views/officers/new.php
@@ -1,13 +1,20 @@
 <?php Section::inject('page_title', 'New Officer') ?>
-<form id="new-officer-form" action="<?php echo e(route('officers')); ?>" method="post">
-  <?php $user = Input::get('user'); ?>
-  <?php $officer = Input::get('officer'); ?>
-  <?php echo View::make('users.account_officer_fields')->with('officer', Input::old('officer'))->with('user', Input::old('user'))->with('signup', true); ?>
-  <div class="form-actions">
-    <div class="control-group form-inline">
+<div class="subheader"></div>
+<div class="container inner-container">
+  <form id="new-officer-form" action="<?php echo e(route('officers')); ?>" method="post">
+    <?php $user = Input::get('user'); ?>
+    <?php $officer = Input::get('officer'); ?>
+
+    <?php echo View::make('users.account_officer_fields')->with('officer', Input::old('officer'))->with('user', Input::old('user'))->with('signup', true); ?>
+
+    <div class="control-group">
       <label>How did you hear about RFP-EZ?</label>
-      <input class="input-xlarge" type="text" name="user[how_hear]" value="" />
+      <input class="input-xxlarge" type="text" name="user[how_hear]" value="" />
     </div>
-    <button class="btn btn-primary" type="submit">Submit</button>
-  </div>
-</form>
+
+    <div>
+      <button class="btn btn-primary" type="submit">Submit</button>
+    </div>
+
+  </form>
+</div>
