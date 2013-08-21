3eb51c48a408690e9808225ae830f5eb036c5832
Aaron Snow <aaron@daringapps.com>
2013-08-21 01:59:20 -0400

    minor bug fix: 404 layout issue

--- application/views/error/404_content.php
+++ application/views/error/404_content.php
@@ -1,2 +1,6 @@
-<?php Section::inject('page_title', "Page not found") ?>
-<p class="lead"><?php echo e(__("r.error.404_content.text")); ?></p>
+<div class="subheader">
+  <?php Section::inject('page_title', "Page not found") ?>
+</div>
+<div class="container inner-container">
+  <p class="lead"><?php echo e(__("r.error.404_content.text")); ?></p>
+</div>
