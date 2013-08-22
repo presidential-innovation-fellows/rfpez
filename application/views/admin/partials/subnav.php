cb66199303218a772c4fd93ea2ffa6b1ee0188a3
Aaron Snow <aaron@daringapps.com>
2013-08-22 17:35:11 -0400

    markup bugfix

--- application/views/admin/partials/subnav.php
+++ application/views/admin/partials/subnav.php
@@ -3,25 +3,26 @@
 </div>
 <div class="container inner-container">
   <div class="navbar navbar-inverse">
-  <div class="navbar-inner">
-    <ul class="nav">
-      <li class="<?php echo e($current_page == 'projects' ? 'active' : ''); ?>">
-        <a href="<?php echo e(route('admin_projects')); ?>">Projects</a>
-      </li>
-      <li class="<?php echo e($current_page == 'officers' ? 'active' : ''); ?>">
-        <a href="<?php echo e(route('admin_officers')); ?>">Officers</a>
-      </li>
-      <li class="<?php echo e($current_page == 'vendors' ? 'active' : ''); ?>">
-        <a href="<?php echo e(route('admin_vendors')); ?>">Vendors</a>
-      </li>
-      <?php if (Auth::officer()->is_role_or_higher(Officer::ROLE_SUPER_ADMIN)): ?>
-        <li class="<?php echo e($current_page == 'emails' ? 'active' : ''); ?>">
-          <a href="<?php echo e(route('admin_emails')); ?>">Emails</a>
+    <div class="navbar-inner">
+      <ul class="nav">
+        <li class="<?php echo e($current_page == 'projects' ? 'active' : ''); ?>">
+          <a href="<?php echo e(route('admin_projects')); ?>">Projects</a>
         </li>
-        <li class="<?php echo e($current_page == 'project_sections' ? 'active' : ''); ?>">
-          <a href="<?php echo e(route('admin_project_sections')); ?>">Project Sections</a>
+        <li class="<?php echo e($current_page == 'officers' ? 'active' : ''); ?>">
+          <a href="<?php echo e(route('admin_officers')); ?>">Officers</a>
         </li>
-      <?php endif; ?>
-    </ul>
+        <li class="<?php echo e($current_page == 'vendors' ? 'active' : ''); ?>">
+          <a href="<?php echo e(route('admin_vendors')); ?>">Vendors</a>
+        </li>
+        <?php if (Auth::officer()->is_role_or_higher(Officer::ROLE_SUPER_ADMIN)): ?>
+          <li class="<?php echo e($current_page == 'emails' ? 'active' : ''); ?>">
+            <a href="<?php echo e(route('admin_emails')); ?>">Emails</a>
+          </li>
+          <li class="<?php echo e($current_page == 'project_sections' ? 'active' : ''); ?>">
+            <a href="<?php echo e(route('admin_project_sections')); ?>">Project Sections</a>
+          </li>
+        <?php endif; ?>
+      </ul>
+    </div>
   </div>
 </div>
