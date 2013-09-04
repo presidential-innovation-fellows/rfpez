8241c9c76266b137d45952edbaf2bd49a0988436
Aaron Snow <aaron@daringapps.com>
2013-09-04 04:05:38 -0400

    One more important checkin required for the new SOW amendment feature.

---
+++ application/migrations/2013_09_03_140904_add_project_amending.php
@@ -0,0 +1,29 @@
+<?php
+
+class Add_Project_Amending {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('projects', function($t){
+			$t->boolean('amending')->default(0);
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('projects', function($t){
+			$t->drop_column('amending');
+		});
+	}
+
+}
